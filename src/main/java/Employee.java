48fbb795eeccba7e4918e774b31d6d997c027fd3
PivIgor <brauer.igor@gmail.com>
2021-03-07 18:08:40 +0300

    homework 5 done

---
+++ src/main/java/Employee.java
@@ -0,0 +1,27 @@
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phone, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void print() {
+        System.out.printf("ФИО: '%s', должность: '%s', E-mail: '%s', телефон: '%s', зарплата: '%f', возраст: '%d'\n",
+                fullName, position, email, phone, salary, age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/Homework05.java
@@ -0,0 +1,47 @@
+public class Homework05 {
+
+    public static void main(String[] args) {
+        performHomework05();
+    }
+
+    private static void performHomework05() {
+
+        Employee[] empArray = new Employee[5];
+        empArray[0] = new Employee("Иванов Иван Иванович",
+                "Инженер",
+                "ivanovii@compani.ru",
+                "+79852347865",
+                80000,
+                25);
+        empArray[1] = new Employee("Пертров Пётр Пертович",
+                "Повар",
+                "petrovpp@compani.ru",
+                "+79999999999",
+                75000,
+                45);
+        empArray[2] = new Employee("Пертрова Анастасия Викторовна",
+                "Бухгалтер",
+                "petrovaav@compani.ru",
+                "+78889854588",
+                50000,
+                35);
+        empArray[3] = new Employee("Пирогов Валерий Сергеевич",
+                "Программист",
+                "pirogovvs@compani.ru",
+                "+7212541215",
+                180000,
+                50);
+        empArray[4] = new Employee("Чичинкин Сергей Сергеевич",
+                "Системный администратор",
+                "chichinkinss@compani.ru",
+                "+72521554111",
+                100000,
+                33);
+
+        for (Employee emp : empArray) {
+            if (emp.getAge() >= 40) {
+                emp.print();
+            }
+        }
+    }
+}
