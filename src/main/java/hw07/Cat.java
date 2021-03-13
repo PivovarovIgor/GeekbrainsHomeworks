431bf70d05a1cbca9e304236ef5cc19b98ad0fe6
PivIgor <brauer.igor@gmail.com>
2021-03-13 22:32:42 +0300

    homework 7 done

---
+++ src/main/java/hw07/Cat.java
@@ -0,0 +1,33 @@
+package hw07;
+
+public class Cat {
+
+    private final String name;
+    private boolean satiety = false;
+    private final int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate p) {
+        if (p.decreaseFood(appetite)) {
+            System.out.println(toString() + " - поел");
+            satiety = true;
+        }
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void info() {
+        System.out.println(name + " " + name + " - " + ((satiety) ? "сытый" : "голодный"));
+    }
+
+}
---
+++ src/main/java/hw07/Plate.java
@@ -0,0 +1,38 @@
+package hw07;
+
+public class Plate {
+
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public boolean decreaseFood(int n) {
+        boolean foodEnough = false;
+        if (food >= n) {
+            food -= n;
+            foodEnough = true;
+        }
+        return foodEnough;
+    }
+
+    public void addFood(int n) {
+        if (n > 0) {
+            food += n;
+        }
+    }
+
+    public void info() {
+        System.out.println("Количество еды в тарелке: " + food);
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+}
